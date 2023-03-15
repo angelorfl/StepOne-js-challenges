bcc425d700365d589d484d3e6618ea6402302d3e
Rafael Angelo <angeloo.rafael.ti@gmail.com>
2023-03-14 22:30:54 -0300

    Aula array

--- 01-objeto.js
+++ 01-objeto.js
@@ -16,3 +16,36 @@
 // 4- Faça um update de um valor! Troque o valor do username para outro valor e depois dê um console.log(user); para ver se a propriedade realmente foi substituída.
 
 // 5- Remova um valor! Remova o valor age e depois dê um *console.log(user);* para ver se a propriedade realmente foi deletada.
+
+
+console.log ('Olá Mundo!');
+
+let user = {
+    username: "Rafael Angelo",
+    email: "mail@mail.com",
+    age: "34 yers",
+};
+
+//login logout
+console.log("Informação de login logout")
+console.log (user.username + " Efetuou o Login");
+console.log (user.username + " Efetuou o Logout");
+
+// Prioridade Age
+console.log("Priorodade para mostar Age")
+console.log(user.age);
+
+//Teste nova propriedade Phone
+console.log("Adicionar propriedade Phone")
+user.phone = "0000-0000"
+console.log(user);
+
+//Update de valor username
+console.log("Update de valor, alterar username")
+user.username = "Angelo Rafael"
+console.log(user.username);
+
+//Remover valor age
+console.log("Deletar valor Age")
+delete user.age
+console.log (user);
--- 02-array.js
+++ 02-array.js
@@ -9,3 +9,34 @@
 // 5- Remova o último elemento do array
 
 // 6- Como ficou o array final?! E qual é o elemento 1 desse array?
+
+console.log("Olá Mundo")
+
+//Criando array vazio
+console.log("Arrey vazio")
+let animals = [];
+console.log(animals);
+
+//adicionando dog e cat no array
+console.log("Adicionando dog e cat no array");
+animals = ["dog", "cat"];
+console.log(animals);
+
+//adicionando no começo do array
+console.log("Adicionando Spider no começo do array");
+animals.unshift("spider");
+console.log(animals);
+
+//remover o primeiro elemento do array
+console.log("Removendo primeiro item do array");
+animals.shift();
+console.log(animals);
+
+//remover ultimo elemento do array
+console.log("Remover ultimo item do array");
+animals.pop();
+console.log(animals);
+
+//resultado do array
+console.log("Resultado final do array")
+console.log(animals)
