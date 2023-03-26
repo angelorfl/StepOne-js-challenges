7aa8eeb9bda9361c89e88f925cfe4eaea7c41de4
Rafael Angelo <angelo.rafael.ti@gmail.com>
2023-03-25 22:33:19 -0300

    while

--- 14-while.js
+++ 14-while.js
@@ -3,3 +3,35 @@
 // Utilizando o while, faça uma estrutura de repetição que dê o console somente dos números pares, começando no 2 e indo até 100.
 
 // Utilizando o while, faça uma estrutura de repetição que dê o console dos números 100 a 1.
+
+console.log("Repetição de 50 a 100") //repetição de  50 a 100
+let i = 50;
+
+while (i<=100){
+    console.log(i);
+    i++;
+}    
+
+console.log("Somente pares de 2 a 100") // repetição de 2 a 100 - somente pares
+
+let n = 1;
+
+while (n <= 100){
+    let resto = n%2;
+    if (resto == 0){
+        console.log(n);
+    }
+    n++;
+}
+
+console.log("Repetição de 100 a 1") // repetição inversa, de 100 a 1
+
+let number = -1;
+
+while (number <= 101){
+    console.log(number);
+    number--;
+    if (number == -101){
+        break;
+    }
+}
